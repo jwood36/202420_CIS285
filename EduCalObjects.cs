16546362b1d91b3cd12140d2249c54d65959545b
Take667 <156635062+Take667@users.noreply.github.com>
2024-02-16 12:38:04 -0600

    update
    I added the s in the end because it was several problems to label as the original name.

---
+++ EduCalObjects.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCal
+{
+    internal class EduCalObjects
+    {
+        //Change Scope: Public
+        public EduCalObjects() { }
+
+
+        //Add Method: ToString()
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+
+
+    }
+}
